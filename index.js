af09ecb43db73f189e07fe399254756f7a3b1090
malaohu <re_ming.ma@hotmail.com>
2017-02-23 16:36:27 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,8 @@
 var http = require('http');
-/*var setup = require('proxy');
+var setup = require('proxy');
  
 var server = setup(http.createServer());
-server.listen(25, function () {
+server.listen((process.env.PORT||8088, function () {
     var port = server.address().port;
     console.log('HTTP(s) proxy server listening on port %d', port);
-});*/
-
-http.createServer(function(req,res){
- res.writeHead(200, {"content-type" : "text/plain"});
-	res.write("hello world");	
-	res.end();
-}).listen(process.env.PORT||3000);
+});
