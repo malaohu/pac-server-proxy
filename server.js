c51199d6fb89391f918bf245098e443b83129039
malaohu <re_ming.ma@hotmail.com>
2017-02-23 14:56:18 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 var http = require('http');
-var setup = require('proxy');
+/*var setup = require('proxy');
  
 var server = setup(http.createServer());
 server.listen(25, function () {
     var port = server.address().port;
     console.log('HTTP(s) proxy server listening on port %d', port);
-});
+});*/
+
+http.createServer(function(req,res){res.write('this is test');res.end();}).listen(8080);
